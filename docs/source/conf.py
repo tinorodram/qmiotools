0909f405bbeccad3329be6946bc1edb004514819
gomeztato <31691116+gomeztato@users.noreply.github.com>
2024-05-20 19:06:55 +0200

    Cambio del Pathen conf.ini

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 from sphinx.ext.autodoc import between
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 def setup(app):
     app.add_config_value('toc_filter_exclude', [], 'html')
